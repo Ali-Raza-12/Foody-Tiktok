8dbcd78fac59b0c4dc90b7c81dba99fd8212f215
Ali-Raza-12 <ar1907311@gmail.com>
2025-09-17 01:22:05 +0500

    Complete authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
 const express = require("express")
+require("dotenv").config();
+const cookiesParser = require("cookie-parser")
+const userRoutes = require('./routes/auth.routes');
+const errorHandler = require("./utills/ErrorHandler");
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cookiesParser());
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+
+app.use("/api/auth", userRoutes)
+
+
+app.use(errorHandler)
 
 module.exports = app;
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,93 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const generateToken = require("../utills/generateToken");
+const sendResponseToken = require("../utills/sendTokenResponse");
+const ApiError = require("../utills/ApiError");
+
+async function registerUser(req, res, next) {
+  try {
+    const { fullName, email, password } = req.body;
+
+    if (!fullName || !email || !password) {
+      throw new ApiError(400, "All fields are required");
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new ApiError(400, "Email already Exist");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = generateToken(user._id);
+    sendResponseToken(res, token);
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function loginUser(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error(400, "All Fields are required")
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error(400, "Invalid Email")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error(400, "Invalid Email or Password")
+    }
+
+    const token = generateToken(user._id);
+    sendResponseToken(res, token);
+
+    return res.status(200).json({
+      message: "Login Successfully",
+      user: {
+        userId: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error)
+  }
+}
+
+async function logoutUser(req, res, next) {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message: "Logout successfully"})
+    } catch (error) {
+        next(error)
+    }
+  
+}
+
+module.exports = { registerUser, loginUser, logoutUser };
